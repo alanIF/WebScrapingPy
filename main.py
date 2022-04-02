183e575ac84dae41ae8d6de887391bbeed9df6ea
Alan Tavares <alan_tj@hotmail.com.br>
2022-04-01 21:47:40 -0300

    read links

--- main.py
+++ main.py
@@ -10,13 +10,38 @@ def get_movie_info(listing_url, links_url, movie_id):
         "diretor": "reginaldo catarino",
         "duracao": "02:12"
     }
+
     html = urlopen(listing_url)
     bs = BeautifulSoup(html, 'html.parser')
     linhas = bs.find_all('tbody')
-    base = read_date(linhas)
-    print(base)
-    
+    base_inicial= read_date(linhas)
 
+    html = urlopen(links_url)
+    bs_links = BeautifulSoup(html, 'html.parser')
+    linhas = bs_links.find_all('tbody')
+    base_links = read_link(linhas)
+    print(base_links)
+def read_link(dados):
+    base = {}
+    k=0
+    x=0
+    filme= {}
+    nome= ""
+    link=""
+    for i in dados:
+        filhas = i.findChildren("td")
+        for j in filhas:  
+                if k == 0:
+                    link= j.text
+                    k+=1
+                else:
+                    nome= j.text
+                    filme={nome : link}
+                    base[x]= filme
+                    x+=1
+                    k=0
+            
+    return base
 def read_date(dados):
     base = {}
     k=0
